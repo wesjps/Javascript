40038fc0696712520c75d60d6db11e3d8918220e
Wesley Silveira <wesleyjps@yahoo.com.br>
2021-11-01 16:41:21 -0300

    Update script.js

--- Jogo Forca/script.js
+++ Jogo Forca/script.js
@@ -1,29 +1,45 @@
-
+/* Elemento HTML referente a categoria */
 const categoria = document.querySelector("#category");
+/* Elemento HTML referente a lista das letras erradas*/
 const letrasErradas = document.querySelector(".wrongLetters");
-const palavraInterface = document.querySelector(".dashes");
+/* Elemento HTML referente a palavra oculta
+   Utilizaremos esse mesmo elemento para exibir as mensagens do jogo*/
+   const palavraInterface = document.querySelector(".dashes");
+/* Array com elementos HTML referentes aos olhos do personagem */
 const olhos = Array.from(document.querySelectorAll(".eyes"));
+/* Array com elementos HTML referentes as partes do corpo */
 let partesBoneco = Array.from(document.querySelectorAll("#person div"));
 partesBoneco = partesBoneco.slice(2, partesBoneco.length);
+/* Palavra corrente */
 let palavraProposta;
+/* Lista das letras erradas */
 let letrasErradasArray = [];
+/* Index da parte do corpo corrente */
 let indiceBoneco;
+/* Numero de chances do jogador */
 const numTentativas = 7;
+/* Valor para opacidade dos olhos */
 const opacidadeOlhos = 0.3;
 
-const categorias = {
-    
-}
 
+/*
+Recebe o evento do teclado e passa apenas o valor da letra para a função tentativa
+*/
 function retornaLetra(e){ 
     tentativa(e.key);
 }
 
+/*
+Desenha a parte do corpo corrente
+*/
 function desenhaBoneco(){
     partesBoneco[indiceBoneco].classList.remove("hide");
     indiceBoneco++; 
 }
 
+/* 
+Desenha os olhos do personagem
+*/
 function desenhaOlhos(){
     olhos.forEach((olho => {
         olho.style.opacity = 1;
@@ -31,6 +47,9 @@ function desenhaOlhos(){
     }));
 }
 
+/*
+Oculta as partes do corpo do personagem
+*/
 function ocultaBoneco(){
     olhos.forEach((olho => {
         olho.style.opacity = opacidadeOlhos; 
@@ -40,6 +59,9 @@ function ocultaBoneco(){
     });
 }
 
+/*
+Inicia as configurações do jogo
+*/
 function iniciaJogo(){
     indiceBoneco = 0;
     letrasErradasArray = [];
