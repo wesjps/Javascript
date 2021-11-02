723d95f3bb6710e2f748155fd510c1ac58570ea9
Wesley Silveira <wesleyjps@yahoo.com.br>
2021-11-02 18:55:10 -0300

    Update app.js

--- TODOLIST/app.js
+++ TODOLIST/app.js
@@ -7,6 +7,18 @@ const paleta1 = document.querySelector("#paleta1");
 const paleta2 = document.querySelector("#paleta2");
 const paleta3 = document.querySelector("#paleta3");
 
+function criaAtividade(){
+    const atividade = document.createElement("div");
+    atividade.classList.add("atividade");
+    const nomeAtividade = document.createElement("p");
+    atividade.textContent = input.value;
+    const botaoLimpar = document.createElement("button");
+    botaoLimpar.textContent = "Limpar";
+    botaoLimpar.classList.add("botao_del");
+    listaAtividades.appendChild(atividade);
+    atividade.appendChild(nomeAtividade);
+    atividade.appendChild(botaoLimpar);
+}
 
 function cadastraAtividade(){
     if(input.value.length > 3){
